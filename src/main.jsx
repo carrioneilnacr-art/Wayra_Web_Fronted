984428ef9534f424a5c75e20946f45b359138559
carrioneilnacr-art <carrioneil.nacr@gmail.com>
2026-04-20 02:00:23 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './assets/index.css' // ✅ Vite procesará este archivo y lo incluirá en el build
+import './assets/index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
